a50bc3d55f53b2e32ad1f86de9c293e6961f08b8
Julian Hoffmann <wi21b050@technikum.at>
2022-06-22 10:18:06 +0200

    Initial commit

---
+++ src/main/java/at/technikum/enzo/EnzoApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.enzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnzoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnzoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/enzo/EnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.enzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnzoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
